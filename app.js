efcb63ef5c66579655564a9fa0e5ccdb3380531c
harshit <chawla>
2019-08-05 13:25:16 +0530

    enable cors

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ require('./util/db');
 
 app.use(morgan('combined'))
 app.use(bodyParser.json());
-
+// cors enable
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use( '/user', userRouter);
 app.listen(port, ()=>{
